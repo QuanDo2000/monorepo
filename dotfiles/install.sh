232fe93b7021bef5f240e1cd6a2d1fb254ce0c6c
Quan Do <minhquand3@gmail.com>
2023-10-04 18:57:13 -0700

    Update install to add python3

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-programs=("zsh" "tmux" "vim" "git" "curl" "awk" "perl" "sed")
+programs=("zsh" "tmux" "vim" "git" "curl" "awk" "perl" "sed" "python3")
 
-dir="~/quan-monorepo/dotfiles"
-olddir="~/dotfiles_old"
+dir=~/quan-monorepo/dotfiles
+olddir=~/dotfiles_old
 files="zshrc vimrc p10k.zsh tmux.conf.local"
 
 # Helper functions
@@ -47,7 +47,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
             if ! check_program "$program"; then
                 echo "Installing $program..."
                 install_program "$distribution" "$program"
-                echo "...done"
             fi
         done
     else
@@ -59,7 +58,6 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     if ! command -v brew &>/dev/null; then
         echo "Homebrew is not installed. Installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"  # Copy-paste from <brew.sh>
-        echo "...done"
     fi
 
     # Install programs
@@ -67,17 +65,15 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         if ! check_program "$program"; then
             echo "Installing $program..."
             brew install "$program"
-            echo "...done"
         fi
     done
 else
     echo "Unsupported operating system. Please install zsh manually."
     exit 1
 fi
-echo "...done"
 
 # Set zsh as default shell
-if [[ "$SHELL" != "/bin/zsh" ]]; then
+if [[ "$SHELL" != /bin/zsh ]]; then
     echo "Setting zsh as default shell..."
     if [[ -x /usr/bin/chsh ]]; then
         sudo chsh -s "$(which zsh)"
@@ -85,33 +81,30 @@ if [[ "$SHELL" != "/bin/zsh" ]]; then
         echo "chsh is not installed. Please set zsh as default shell manually."
         exit 1
     fi
-    echo "...done"
 fi
 
 # Create dotfiles_old in ~
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # Move to dotfiles directory
 echo "Move to $dir directory"
 cd $dir
-echo "...done"
 
 # Installing for oh-my-zsh. This needs to go before the links.
-if [[ -d "~/.oh-my-zsh" ]]; then
+if [[ -d ~/.oh-my-zsh ]]; then
   echo "An existing oh-my-zsh installation detected."
 else
   echo "Installing oh-my-zsh..."
   # https://github.com/ohmyzsh/ohmyzsh/wiki
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # https://github.com/ohmyzsh/ohmyzsh#unattended-install
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   # https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   # https://github.com/romkatv/powerlevel10k#installation
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  echo "...done"
 fi
 
 # Installing Oh My Tmux.
@@ -122,7 +115,7 @@ ln -s -f .tmux/.tmux.conf
 
 # Move existing dotfiles to old directory, then create symlinks
 for file in $files; do
-    if [[ -f "~/.$file" ]]; then
+    if [[ -f ~/.$file ]]; then
         echo "Moving existing .$file from ~ to $olddir"
         mv ~/.$file $olddir
     fi

6e2ad3651c599add73e2e577f6de0a4e3f819566
Quan Do <minhquand3@gmail.com>
2023-11-04 02:59:44 -0700

    Add font installation.

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 
 ## dotfiles
 
-Contained within `dotfiles` are my personal setup scripts for a new machine. To install, run the following:
+Contained within `dotfiles` are my personal setup scripts for a new machine.
+
+- To install on Linux/MacOS, run the following:
 
 ```bash
 source <(curl -s https://raw.githubusercontent.com/QuanDo2000/monorepo/main/dotfiles/install)
+```
+
+- To install on Windows, run the following in PowerShell as Administrator:
+
+```powershell
+Set-ExecutionPolicy Unrestricted -Scope Process
+iwr -useb https://raw.githubusercontent.com/QuanDo2000/monorepo/main/dotfiles/install.ps1 | iex
 ```
--- dotfiles/install.ps1
+++ dotfiles/install.ps1
@@ -1,19 +1,82 @@
-winget install Microsoft.Powershell Git.Git JanDeDobbeleer.OhMyPosh vim.vim Microsoft.VisualStudioCode Microsoft.WindowsTerminal --disable-interactivity
 
-Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force
-Install-Module -Name PSReadLine -Scope CurrentUser -Force
+function CopyWithBackup($source, $destination) {
+    if (!(Test-Path $destination)) {
+        Copy-Item -Path $source -Destination $destination
+    }
+    else {
+        Copy-Item -Path $destination -Destination "$destination.bak"
+        Copy-Item -Path $source -Destination $destination -Force
+    }
+}
 
-Update-Module
+function InstallPackages() {
+    winget install Microsoft.Powershell Git.Git JanDeDobbeleer.OhMyPosh vim.vim Microsoft.VisualStudioCode Microsoft.WindowsTerminal --disable-interactivity
 
-if (!(Test-Path $HOME\Documents\WindowsPowerShell)) { New-Item -ItemType Directory -Path $HOME\Documents\WindowsPowerShell }
-if (!(Test-Path $HOME\Documents\PowerShell)) { New-Item -ItemType Directory -Path $HOME\Documents\PowerShell }
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force
+    Install-Module -Name PSReadLine -Scope CurrentUser -Force
 
-cmd /c mklink $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 $PSScriptRoot\windows\Powershell\Microsoft.PowerShell_profile.ps1
-cmd /c mklink $HOME\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1 $PSScriptRoot\windows\Powershell\Microsoft.VSCode_profile.ps1
-cmd /c mklink $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 $PSScriptRoot\windows\Powershell\Microsoft.PowerShell_profile.ps1
-cmd /c mklink $HOME\Documents\PowerShell\Microsoft.VSCode_profile.ps1 $PSScriptRoot\windows\Powershell\Microsoft.VSCode_profile.ps1
+    Update-Module
+}
 
-cmd /c mklink $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $PSScriptRoot\windows\Terminal\settings.json
+function InstallFont {
+    $url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip"
+    $tempFile = "$env:TEMP\FiraCode.zip"
+    $fontFolder = "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"
 
-cmd /c mklink $HOME\_vimrc $PSScriptRoot\vimrc.symlink
-cmd /c mklink $HOME\_gvimrc $PSScriptRoot\windows\_gvimrc
+    Write-Host "Downloading FiraCode Nerd Font..."
+    Invoke-WebRequest -Uri $url -OutFile $tempFile
+
+    Write-Host "Extracting FiraCode Nerd Font..."
+    Expand-Archive -Path $tempFile -DestinationPath $fontFolder
+
+    Write-Host "Installing FiraCode Nerd Font..."
+    $shellApplication = New-Object -ComObject Shell.Application
+    $fontFiles = Get-ChildItem -Path $fontFolder -Filter "*.ttf"
+
+    foreach ($fontFile in $fontFiles) {
+        $fontName = $fontFile.Name
+        $fontPath = $fontFile.FullName
+
+        if (!(Test-Path $fontPath)) {
+            $shellApplication.Namespace($fontFolder).ParseName($fontName).InvokeVerb("Install")
+        }
+    }
+
+    Write-Host "Cleaning up..."
+    Remove-Item -Path $tempFile
+
+    Write-Host "FiraCode Nerd Font installed successfully."
+}
+
+function SyncSettings() {
+    $targets = @(
+        "$HOME\Documents\WindowsPowerShell"
+        "$HOME\Documents\PowerShell"
+    )
+
+    foreach ($target in $targets) {
+        if (!(Test-Path $target)) {
+            New-Item -ItemType Directory -Path $target
+        }
+    }
+
+    foreach ($target in $targets) {
+        Get-ChildItem -Path $PSScriptRoot\windows\Powershell -File | ForEach-Object {
+            $file = $_
+            $filename = $_.Name
+            $destination = "$target\$filename"
+            CopyWithBackup -source $file -destination $destination
+        }
+    }
+
+    $terminalSettingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+    $terminalSettingsSource = "$PSScriptRoot\windows\Terminal\settings.json"
+    CopyWithBackup -source $terminalSettingsSource -destination $terminalSettingsPath
+
+    CopyWithBackup -source "$PSScriptRoot\vimrc.symlink" -destination "$HOME\_vimrc"
+    CopyWithBackup -source "$PSScriptRoot\windows\_gvimrc" -destination "$HOME\_gvimrc"
+}
+
+# InstallPackages
+InstallFont
+# SyncSettings
